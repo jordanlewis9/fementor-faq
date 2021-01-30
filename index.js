b4d1235e47bd88daa79836dd5e424dee225d6ff6
Jordan <jlewis2008@live.com>
2021-01-30 14:56:40 -0500

    mobile layout finished

--- index.js
+++ index.js
@@ -15,25 +15,41 @@ const toggleAccordion = (e) => {
   let clickedSVG = document.querySelector(`.accordion__svg--${clickedTargetNumber}`);
   if ( currentTarget === 0 ) {
     clickedSection.classList.add('is_selected');
-    clickedSVG.classList.add('svg_selected');
+    clickedSVG.classList.add('svg_transition_up');
     clickedTarget.classList.add('header__selected')
     currentTarget = clickedTarget.dataset.tab
+    setTimeout(() => {
+      clickedSVG.classList.add('svg_selected');
+      clickedSVG.classList.remove('svg_transition_up');
+    }, 290)
   } else if (currentTarget === clickedTargetNumber) {
     clickedSection.classList.remove('is_selected');
-    clickedSVG.classList.remove('svg_selected');
+    clickedSVG.classList.add('svg_transition_down');
     clickedTarget.classList.remove('header__selected');
     currentTarget = 0;
+    setTimeout(() => {
+      clickedSVG.classList.remove('svg_selected');
+      clickedSVG.classList.remove('svg_transition_down');
+    }, 290)
   } else {
     let oldSection = document.querySelector(`.accordion__content--${currentTarget}`);
     let oldSVG = document.querySelector(`.accordion__svg--${currentTarget}`);
     let oldTarget = document.querySelector(`.accordion__header--${currentTarget}`);
     oldSection.classList.remove('is_selected');
-    oldSVG.classList.remove('svg_selected');
+    oldSVG.classList.add('svg_transition_down');
+    // oldSVG.classList.remove('svg_selected');
     oldTarget.classList.remove('header__selected');
     clickedSection.classList.add('is_selected');
-    clickedSVG.classList.add('svg_selected');
+    clickedSVG.classList.add('svg_transition_up');
+    // clickedSVG.classList.add('svg_selected');
     clickedTarget.classList.add('header__selected');
     currentTarget = clickedTargetNumber;
+    setTimeout(() => {
+      oldSVG.classList.remove('svg_selected');
+      oldSVG.classList.remove('svg_transition_down');
+      clickedSVG.classList.remove('svg_transition_up');
+      clickedSVG.classList.add('svg_selected');
+    }, 290)
   }
 }
 
