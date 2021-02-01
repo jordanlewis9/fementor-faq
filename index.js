71182ff5496a50d9678f026a0f00cee9e8920528
Jordan <jlewis2008@live.com>
2021-02-01 15:15:56 -0500

    mobile layout complete

--- index.js
+++ index.js
@@ -11,45 +11,53 @@ let currentTarget = 0;
 const toggleAccordion = (e) => {
   const clickedTarget = e.target.closest('.accordion__header');
   const clickedTargetNumber = clickedTarget.dataset.tab;
-  let clickedSection = document.querySelector(`.accordion__content--${clickedTargetNumber}`);
+  let clickedContent = document.querySelector(`.accordion__content--${clickedTargetNumber}`);
   let clickedSVG = document.querySelector(`.accordion__svg--${clickedTargetNumber}`);
   if ( currentTarget === 0 ) {
-    clickedSection.classList.add('is_selected');
+    clickedTarget.classList.add('header_selected');
+    clickedContent.classList.add('accordion_content_reveal');
+    clickedContent.classList.remove('accordion__content');
     clickedSVG.classList.add('svg_transition_up');
-    clickedTarget.classList.add('header__selected')
-    currentTarget = clickedTarget.dataset.tab
+    currentTarget = clickedTarget.dataset.tab;
     setTimeout(() => {
+      clickedContent.classList.remove('accordion_content_reveal');
+      clickedContent.classList.add('is_selected');
       clickedSVG.classList.add('svg_selected');
       clickedSVG.classList.remove('svg_transition_up');
-    }, 290)
+    }, 149)
   } else if (currentTarget === clickedTargetNumber) {
-    clickedSection.classList.remove('is_selected');
+    clickedContent.classList.remove('is_selected');
+    clickedContent.classList.add('accordion__content');
     clickedSVG.classList.add('svg_transition_down');
-    clickedTarget.classList.remove('header__selected');
+    clickedTarget.classList.remove('header_selected');
     currentTarget = 0;
     setTimeout(() => {
       clickedSVG.classList.remove('svg_selected');
       clickedSVG.classList.remove('svg_transition_down');
-    }, 290)
+    }, 149)
   } else {
-    let oldSection = document.querySelector(`.accordion__content--${currentTarget}`);
+    let oldContent = document.querySelector(`.accordion__content--${currentTarget}`);
     let oldSVG = document.querySelector(`.accordion__svg--${currentTarget}`);
     let oldTarget = document.querySelector(`.accordion__header--${currentTarget}`);
-    oldSection.classList.remove('is_selected');
+    oldContent.classList.remove('is_selected');
+    oldContent.classList.add('accordion__content');
     oldSVG.classList.add('svg_transition_down');
     // oldSVG.classList.remove('svg_selected');
-    oldTarget.classList.remove('header__selected');
-    clickedSection.classList.add('is_selected');
+    oldTarget.classList.remove('header_selected');
+    clickedContent.classList.remove('accordion__content');
+    clickedContent.classList.add('accordion_content_reveal');
     clickedSVG.classList.add('svg_transition_up');
     // clickedSVG.classList.add('svg_selected');
-    clickedTarget.classList.add('header__selected');
+    clickedTarget.classList.add('header_selected');
     currentTarget = clickedTargetNumber;
     setTimeout(() => {
       oldSVG.classList.remove('svg_selected');
       oldSVG.classList.remove('svg_transition_down');
+      clickedContent.classList.add('is_selected');
+      clickedContent.classList.remove('accordion_content_reveal');
       clickedSVG.classList.remove('svg_transition_up');
       clickedSVG.classList.add('svg_selected');
-    }, 290)
+    }, 149)
   }
 }
 
